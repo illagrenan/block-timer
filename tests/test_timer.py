10f15f6cdbf57067b47d4d303c3e13e9f31f40b9
Vašek Dohnal <vaclav.dohnal@gmail.com>
2016-12-02 14:41:01 +0100

    More tests.

--- tests/test_timer.py
+++ tests/test_timer.py
@@ -1,14 +1,16 @@
 # -*- encoding: utf-8 -*-
 # ! python3
 
+import io
+import re
 import time
+from contextlib import redirect_stdout
 from unittest import TestCase
 
 from block_timer.timer import Timer
 
 
 class BasicTestCase(TestCase):
-    # noinspection PyMethodMayBeStatic
     def test_basic(self):
         with Timer(title="Some title") as t:
             time.sleep(1 / 3)
@@ -17,12 +19,38 @@ class BasicTestCase(TestCase):
         self.assertIsInstance(str(t), str)
         self.assertIsInstance(repr(t), str)
         self.assertIsInstance(float(t), float)
-        self.assertGreaterEqual(t.elapsed, 1 / 3)  # a >= b
+        self.assertGreaterEqual(t.elapsed, 1 / 4)  # a >= b
 
         self.assertTrue(hasattr(t, 'elapsed'))
         self.assertIsInstance(t.elapsed, float)
 
         self.assertEqual(float(t), t.elapsed)
+
+    def test_multiple_instances(self):
+        with Timer(title="Some title") as t1:
+            time.sleep(1 / 5)
+
+        with Timer(title="Some title") as t2:
+            time.sleep(1 / 5)
+
+        self.assertIsNot(t1, t2)
+
+    def test_stdout(self):
+        with io.StringIO() as buffer1, redirect_stdout(buffer1):
+            with Timer(title="Some title"):
+                time.sleep(1 / 5)
+
+            output1 = buffer1.getvalue()
+
+        self.assertRegex(output1, re.compile(r"\[Some title\] Total time \d+\.\d+ seconds.\n", re.UNICODE))
+
+        with io.StringIO() as buffer2, redirect_stdout(buffer2):
+            with Timer(title="Different title"):
+                time.sleep(1 / 5)
+
+            output2 = buffer2.getvalue()
+
+        self.assertRegex(output2, re.compile(r"\[Different title\] Total time \d+\.\d+ seconds.\n", re.UNICODE))
 
     def test_decorator(self):
         @Timer()
