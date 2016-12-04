e674c2e8c27ca0dc8cd9085c27ec59f0ab0d3722
Vašek Dohnal <vaclav.dohnal@gmail.com>
2016-12-04 17:55:03 +0100

    New release.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name='block-timer',
-    version='0.1.1',
+    version='0.1.2',
     description="""Measure execution time of your code blocks""",
     long_description=io.open("README.rst", 'r', encoding="utf-8").read(),
     url='https://github.com/illagrenan/block-timer',
